5b283c51069111354fc2c9f2d9c935d44ccad120
elisate <dushimiyimanaelisa@gmail.com>
2026-02-05 01:36:22 +0200

    fix all images crud

--- src/Routes/newsRoute.ts
+++ src/Routes/newsRoute.ts
@@ -6,7 +6,7 @@ import { upload } from '../middlewares/multer';
 const newsRouter = Router();
 
 // 'images' is the key name for Postman
-newsRouter.post('/createNews ', upload.array('images', 10), createNews); 
+newsRouter.post('/createNews', upload.array('images', 10), createNews); 
 newsRouter.get('/getAllNews', getAllNews);
 newsRouter.delete('/deleteNews/:id', deleteNews);
 // in src/routes/news.routes.ts
