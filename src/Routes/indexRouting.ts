43a956e12399855eeae1b698096c2925de73f412
elisate <dushimiyimanaelisa@gmail.com>
2026-02-05 01:01:01 +0200

    fix all chnages in the program content

--- src/Routes/indexRouting.ts
+++ src/Routes/indexRouting.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRouter from "./authRoutes";
-
+import programRoutes from "./programRoutes";
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
+mainRouter.use("/program", programRoutes);
 
 export default mainRouter;
